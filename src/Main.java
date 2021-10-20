de95a669e3309a882918ac8c68855ce7318ebf5a
Alfred Roos <alfred@stensatter.se>
2021-10-20 09:06:31 +0200

    Added help command

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import jdk.nashorn.internal.runtime.ECMAException;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,6 +14,8 @@ public class Main {
     static HashMap<String,Float> vars = new HashMap<String, Float>();
     public static void main(String[] args)
     {
+        System.out.println("Type help for help");
+
         while(true){
             BufferedReader reader = new BufferedReader(
                     new InputStreamReader(System.in));
@@ -30,10 +34,26 @@ public class Main {
                if(!vars.containsKey(var))
                     vars.put(var,Float.parseFloat(pp.getAnswer(InfixToPostfix.infixToPostfix((exp.split("=")[1])))));
            }
+           else if(exp.equals("help"))
+           {
+               System.out.println("If you want to set variables write 'x=5*5' with anny letter (not words) ");
+               System.out.println("Addition is with +");
+               System.out.println("Subtraction is with -");
+               System.out.println("Multiplication is with *");
+               System.out.println("Power of is with ^");
+               System.out.println("Scare root is with !");
+
+           }
            else
            {
-               Map<String,Float> m = vars;
-               System.out.println(pp.getAnswer(InfixToPostfix.infixToPostfix(Maths.switchVariable(exp,m))));
+               try{
+                    Map<String,Float> m = vars;
+                    System.out.println(pp.getAnswer(InfixToPostfix.infixToPostfix(Maths.switchVariable(exp,m))));
+                }
+               catch(Exception e)
+               {
+                   System.out.println("Syntax error");
+               }
            }
                 //System.out.println((Maths.switchVariable(exp,"x",x)));
         }
